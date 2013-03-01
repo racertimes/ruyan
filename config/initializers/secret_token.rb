141c14fe3985069816f7ffb2b513a62225d77b60
alibushehri <ali@racertimes.com>
2013-03-01 12:51:31 +0300

    created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Racebook::Application.config.secret_token = '98e821c8a5e6a28ef21f80325ea9fa61517093ebca55c4f5533d978eba015e312df54a0312ecc2c0adfc0f5e83afa223baf6d107cccbc049a7468c8e1ec68ad3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Racebook::Application.config.session_store :cookie_store, key: '_racebook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Racebook::Application.config.session_store :active_record_store
